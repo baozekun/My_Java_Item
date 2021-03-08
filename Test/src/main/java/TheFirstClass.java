90bd016e89c5091d94d166e6fbd7fb87b17a3637
baozekun <1147468701@qq.com>
2021-03-08 17:24:41 +0800

    一个测试项目

---
+++ Test/src/main/java/TheFirstClass.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：bzk
+ * @date ：Created in 2021/3/8 17:22
+ * @description：测试git好用不
+ * @modified By：
+ */
+public class TheFirstClass  {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
